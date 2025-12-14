b9f885f934caec784df6743a186fb514b72a09ef
philippart-s <wildagsx@gmail.com>
2025-12-14 19:14:32 +0000

    feat: 🎤 conference page generation

---
+++ src/main/java/fr/wilda/blog/generator/ConferenceGenerator.java
@@ -0,0 +1,59 @@
+package fr.wilda.blog.generator;
+
+import io.quarkus.logging.Log;
+import io.quarkus.runtime.StartupEvent;
+import io.vertx.core.json.JsonObject;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+@ApplicationScoped
+public class ConferenceGenerator {
+    @Inject
+    @Named("conferences")
+    JsonObject conferences;
+
+    String frontMatter = """
+---
+title: "🎤 Talks donnés à %s 🎤"
+description: Liste de talks donnés lors de la conférence %s
+image: %s
+layout: conference
+author: wilda
+conference-name: %s
+link: %s
+---""";
+
+    /** Méthode appelée dès que l’application est prête. */
+    void onStart(@Observes StartupEvent ev) throws IOException {
+        Log.info("🚀 Conference pages generation...");
+        JsonObject conference = conferences.getJsonArray("2025").getJsonObject(0);
+
+        Path dir = Path.of("./content/posts/conferences/" + conference.getString("postDate") + "-" + conference.getString("talksUrl"));
+        if (!Files.isDirectory(dir)) {
+            Files.createDirectories(dir);
+        }
+        Path file = Path.of(dir + "/index.markdown");
+        if (!Files.exists(file)) {
+            Files.createDirectories(dir);
+            Files.write(file,
+                    frontMatter.formatted(conference.getString("name"),
+                            conference.getString("name"),
+                            (Files.exists(Path.of("./public/images/" + conference.getString("talksUrl") + ".png")) ?
+                                    conference.getString("talksUrl") + ".png" :
+                                    "conference.jpg"),
+                            conference.getString("talksUrl"),
+                            conference.getString("talksUrl")).getBytes(),
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.TRUNCATE_EXISTING,
+                    StandardOpenOption.WRITE);
+        }
+        Log.info("✅ Conference pages generated ✅");
+    }
+}
